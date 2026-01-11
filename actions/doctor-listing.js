2ff81beb94fd77064fc467d5bcd3655fef4bf86f
Rahul <rahuldevan2019@gmail.com>
2026-01-12 00:39:39 +0530

    fetch all doctors almost completed

---
+++ actions/doctor-listing.js
@@ -0,0 +1,23 @@
+import { db } from '@/lib/prisma';
+
+async function getDoctorBySpecialty(specialty) {
+    try {
+        const doctors = await db.user.findMany({
+            where: {
+                role: "DOCTOR",
+                verificationStatus: "VERIFIED",
+                specialty: specialty.split("%20").join(" "),
+            },
+            orderBy: {
+                name: "asc",
+            },
+        });
+
+        return { doctors };
+    } catch (error) {
+        console.error("Failed to fetch doctors by specialty:", error);
+        return { error: "Failed to fetch doctors" };
+    }
+}
+
+export default getDoctorBySpecialty
