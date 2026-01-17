0d1e4647afa351012fef9dbd037f0cda198c0723
Rahul <rahuldevan2019@gmail.com>
2026-01-18 00:53:08 +0530

    completed payout apprve or reject by admin

--- app/(main)/admin/page.jsx
+++ app/(main)/admin/page.jsx
@@ -1,13 +1,16 @@
-import { getPendingDoctos, getVerifiedDoctors } from '@/actions/admin'
+import { getPendingDoctos, getPendingPayouts, getVerifiedDoctors } from '@/actions/admin'
 import { TabsContent } from '@/components/ui/tabs'
 import React from 'react'
 import PendingDoctors from './_components/pending-doctors'
+import { PendingPayouts } from './_components/pending-payouts'
+import VerifiedDoctors from './_components/verified-doctors'
 
 export default async function AdminPage() {
     //fetch all data in parallel
-    const [pendingDoctorsData, verifiedDoctorsData] = await Promise.all([
+    const [pendingDoctorsData, verifiedDoctorsData, pendingPayoutsData] = await Promise.all([
         getPendingDoctos(),
         getVerifiedDoctors(),
+        getPendingPayouts(),
     ])
     return (
         <>
@@ -16,7 +19,11 @@ export default async function AdminPage() {
             </TabsContent>
 
             <TabsContent value="doctors" className="border-none p-0">
-                <PendingDoctors doctors={verifiedDoctorsData.doctors || []} />
+                <VerifiedDoctors doctors={verifiedDoctorsData.doctors || []} />
+            </TabsContent>
+
+            <TabsContent value="payouts" className="border-none p-0">
+                <PendingPayouts payouts={pendingPayoutsData.payouts || []} />
             </TabsContent>
         </>
     )
